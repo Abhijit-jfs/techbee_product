756bb6e64d6a915e36a05f5f1e9b8ed4a6526570
Abhijit Parida <59194649+Abhijit-jfs@users.noreply.github.com>
2020-01-13 14:05:54 +0530

    Update Product_scripts.sql

--- Product_scripts.sql
+++ Product_scripts.sql
@@ -1,17 +1,17 @@
 create table TProduct(
-product_id VARCHAR(8),
-product_name VARCHAR(128),
-product_description VARCHAR(1024),
-product_price NUMBER,
-product_rating NUMBER
+  product_id VARCHAR(8),
+  product_name VARCHAR(128),
+  product_description VARCHAR(1024),
+  product_price NUMBER,
+  product_rating NUMBER
 );
 
 create table TOrder(
-order_id VARCHAR(8),
-product_id VARCHAR(8),
-customer_id  VARCHAR(8),
-cart_value number,
-coupon_code VARCHAR(16)
+  order_id VARCHAR(8),
+  product_id VARCHAR(8),
+  customer_id  VARCHAR(8),
+  cart_value number,
+  coupon_code VARCHAR(16)
 );
 
 insert into TProduct values('P001', 'Architects of Intelligence', 'How will AI evolve and what major innovations are on the horizon? ', 19.9, 4.6);
