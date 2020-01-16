9483305f309a3b27e842a94751ff0dc29f07d716
Abhijit Parida <59194649+Abhijit-jfs@users.noreply.github.com>
2020-01-16 10:01:28 +0530

    Add models

---
+++ model/Product.java
@@ -0,0 +1,60 @@
+package com.techbee.model;
+
+public class Product {
+
+	private int productId;
+	private String productName;
+	private String productDescription;
+	private double productPrice;
+	private double productRating;
+	
+	public Product() {
+		
+	}
+	
+	public int getProductId() {
+		return productId;
+	}
+	
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	
+	public String getProductName() {
+		return productName;
+	}
+	
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	public String getProductDescription() {
+		return productDescription;
+	}
+	
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	public double getProductPrice() {
+		return productPrice;
+	}
+	
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	public double getProductRating() {
+		return productRating;
+	}
+	
+	public void setProductRating(double productRating) {
+		this.productRating = productRating;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productDescription="
+				+ productDescription + ", productPrice=" + productPrice + ", productRating=" + productRating + "]";
+	}
+}
